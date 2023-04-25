0a854432dc834a8ea4c016148fc002d87a5d22f8
Dubau-Horea-Filip <filipdubau@yahoo.ro>
2023-04-25 13:37:05 +0300

    TakeHome3

--- src/test/java/org/example/TemaTests.java
+++ src/test/java/org/example/TemaTests.java
@@ -2,12 +2,14 @@ package org.example;
 
 import domain.Student;
 import domain.Tema;
+import org.junit.jupiter.api.Assertions;
 import org.testng.annotations.Test;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
 import service.Service;
 import validation.StudentValidator;
 import validation.TemaValidator;
+import validation.ValidationException;
 import validation.Validator;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -30,5 +32,40 @@ public class TemaTests {
     public void InvalidTemaDeadline() {
         int response = service.saveTema("99", "test", 20, 6);
         assertEquals(1, response);
+    }
+
+    @Test
+    public void AddAssignmentIdNull() {
+        int response = service.saveTema(null, "Descrption1", 8, 6);
+        assertEquals(1, response);
+    }
+
+    @Test
+    public void AddAssignmentDescriptionNull() {
+        int response = service.saveTema("A1", null, 15, 5);
+        assertEquals(1, response);
+    }
+
+    @Test
+    public void AddAssignmentDescriptionEmpty() {
+        int response = service.saveTema("A1", "", 7, 5);
+        assertEquals(1, response);
+    }
+
+    @Test
+    public void AddAssignmentDeadline15() {
+
+
+        int response = service.saveTema("A1", "Descrption1", 15, 5);
+        assertEquals(1, response);
+
+    }
+
+    @Test
+    public void AddAssignmentReceivingWeek0() {
+
+        int response = service.saveTema("A1", "Descrption1", 7, 0);
+        assertEquals(1, response);
+
     }
 }
